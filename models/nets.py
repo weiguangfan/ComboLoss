d5d99f7c39978fbbf316ad41850935d95887d93b
weiguangfan <826008728@qq.com>
2022-07-29 11:53:33 +0800

    add comment

--- models/nets.py
+++ models/nets.py
@@ -46,20 +46,26 @@ class ComboNet(nn.Module):
             for name, module in self.backbone.named_children():
                 # 验证模块名称是否等于字符串'fc'
                 if name != 'fc':
-                    # 将输入数据填入模块对象
+                    # 将输入数据填入模块对象，返回一个tensor
+                    # Linear(in_features=512, out_features=1000, bias=True)
                     x = module(x)
-            #
+            # 调用类方法self.num_flat_features():返回张量的元素总数
+            # Tensor.view():返回一个新的张量，其数据与自我张量相同，但形状不同。
             feat = x.view(-1, self.num_flat_features(x))
-
+        # 调用torch.nn.Linear(in_features,out_features):对传入的数据进行线性转换:y = xA^T + b
         regression_output = self.regression_branch(feat)
+        # 调用torch.nn.Linear(in_features,out_features):对传入的数据进行线性转换:y = xA^T + b
         classification_output = self.classification_branch(feat)
 
         return regression_output, classification_output
 
     def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
+        # Tensor.size():返回自我张量的大小。
+        size = x.size()[1:]  # 所有的维度，除了批次维度
         num_features = 1
+        # 遍历各个维度的值
         for s in size:
+            # 计算元素的总数
             num_features *= s
-
+        # 返回张量的元素的总数
         return num_features
